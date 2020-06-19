421a2b4d1125ca8053d9efd39b10cd6956d9e768
Amber Hsueh <ting820831@gmail.com>
2020-06-19 17:39:26 -0500

    adding css & js

---
+++ script.js
@@ -0,0 +1,42 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".rwo .seat:not(.occupied)");
+// querySelectorAll -> array
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+// const ticketPrice1 = parseInt(movieSelect.value);
+// console.log(typeof ticketPrice);
+
+//function
+//update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  console.log(selectedSeats);
+  const selectedSeatsCount = selectedSeats.length;
+  console.log(selectedSeatsCount);
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+//seat click event
+container.addEventListener("click", (e) => {
+  //console.log(e.target); element that clicked on
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    // console.log(e.target);
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
